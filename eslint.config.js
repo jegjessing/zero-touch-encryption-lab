99743b84e9763dfcf0095e47b92d85cc60ff2039
Jarl Gjessing <jarlgjessing@gmail.com>
2026-08-31 21:19:30 +0200

    Missed the mjs and cjs in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ export default [
   },
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
